6dbc37cdc4992fce7c106b3a2668c683730355f2
Ritesh Kumar <ritesh705@gmail.com>
2021-08-19 19:19:22 +0530

    initial commt

---
+++ src/main/java/com/ritesh/soapws/SoapWSApp.java
@@ -0,0 +1,12 @@
+package com.ritesh.soapws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWSApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWSApp.class, args);
+	}
+}
